073d3b65efa6b79ac814770ac818db54fb0437bc
Shariq Shaikh <sshaikh524@gmail.com>
2015-02-19 10:03:18 -0500

    working on laptop today

--- app/src/main/java/com/shaikhapps/circuittrainingtimer/CreateTimersActivity.java
+++ app/src/main/java/com/shaikhapps/circuittrainingtimer/CreateTimersActivity.java
@@ -2,8 +2,6 @@ package com.shaikhapps.circuittrainingtimer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,8 +11,10 @@ import android.widget.Toast;
 public class CreateTimersActivity extends ActionBarActivity {
 
     public static final String TAG = CreateTimersActivity.class.getSimpleName();
-    private int timerIntAtOnes;
-    private int timerIntAtTens;
+
+    private int hourValue;
+    private int hourOnesValue;
+    private int hourTensValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,46 +25,42 @@ public class CreateTimersActivity extends ActionBarActivity {
         Button hourDownButton = (Button) findViewById(R.id.hourDownButton);
 
         timer.setText("00:00:00");
+        hourOnesValue = Character.getNumericValue(timer.getText().charAt(1));
+        hourTensValue = Character.getNumericValue(timer.getText().charAt(0))*10;
 
-        timerIntAtOnes = Character.getNumericValue(timer.getText().charAt(1));
-        timerIntAtTens = Character.getNumericValue(timer.getText().charAt(0));
         hourUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*if (timerIntAtOnes < 9 ) {
-                    timerIntAtOnes++;
+
+                hourValue = hourTensValue  + hourOnesValue;
+                if (hourValue < 12){
+                    hourValue++;
                 }
                 else{
-                    timerIntAtOnes = 0;
-                    timerIntAtTens++;
+                    hourValue = 0;
                 }
-                if(timerIntAtTens > 0 && timerIntAtOnes > 2 ){
-                    timerIntAtOnes = 0;
-                    timerIntAtTens = 0;
-                }*/
 
-
-                timer.setText(String.valueOf(timerIntAtTens) + String.valueOf(timerIntAtOnes) + ":00:00");
-                //Toast.makeText(getBaseContext(), String.valueOf(timerIntAtTens) + "," + String.valueOf(timerIntAtOnes), Toast.LENGTH_SHORT).show();
+                //timer.setText(String.valueOf(hourTensValue) + String.valueOf(hourOnesValue) + ":00:00");
+                Toast.makeText(getBaseContext(), String.valueOf(hourValue), Toast.LENGTH_SHORT).show();
             }
         });
-        hourDownButton.setOnClickListener(new View.OnClickListener() {
+        /*hourDownButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(timerIntAtTens == 1 && timerIntAtOnes == 0){
-                    timerIntAtTens = 0;
-                    timerIntAtOnes = 9;
+                if(hourTensValue == 1 && hourOnesValue == 0){
+                    hourTensValue = 0;
+                    hourOnesValue = 9;
                 }
-                if(timerIntAtTens < 1 && timerIntAtOnes < 1){
-                    timerIntAtOnes = 2;
-                    timerIntAtTens = 1;
+                if(hourTensValue < 1 && hourOnesValue < 1){
+                    hourOnesValue = 2;
+                    hourTensValue = 1;
                 }
                 else{
-                    timerIntAtOnes--;
+                    hourOnesValue--;
                 }
-                timer.setText(String.valueOf(timerIntAtTens) + String.valueOf(timerIntAtOnes) + ":00:00");
+                timer.setText(String.valueOf(hourTensValue) + String.valueOf(hourOnesValue) + ":00:00");
             }
-        });
+        });*/
 
     }
 }
